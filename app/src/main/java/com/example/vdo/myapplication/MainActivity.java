e14a12f74fd13f651fc8121af5e6a1b701829aa9
vdo <362952519@github.com>
2016-07-12 00:19:12 -0700

    add a log

--- app/src/main/java/com/example/vdo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/vdo/myapplication/MainActivity.java
@@ -5,9 +5,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Log.d("lrm","hello");
             }
         });
     }
