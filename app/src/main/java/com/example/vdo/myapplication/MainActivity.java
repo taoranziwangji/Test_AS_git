1a95540af1ff18aa592fb46dcdd8ee866efa33de
vdo <362952519@qq.com>
2016-07-12 00:20:43 -0700

    Update MainActivity.java
    edit log ,add "world"

--- app/src/main/java/com/example/vdo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/vdo/myapplication/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                Log.d("lrm","hello");
+                Log.d("lrm","hello world");
             }
         });
     }
